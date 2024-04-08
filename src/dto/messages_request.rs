b076edc4be63d4591741314c708710a452bf705b
averichev <averichev@yandex.ru>
2024-04-08 11:49:34 +0300

    fix: Префиксы в DTO

--- src/dto/messages_request.rs
+++ src/dto/messages_request.rs
@@ -7,34 +7,25 @@ namespace = "soapenv: http://schemas.xmlsoap.org/soap/envelope/"
 namespace = "ns: urn://x-artefacts-gnivc-ru/inplat/servin/OpenApiAsyncMessageConsumerService/types/1.0"
 )]
 pub(crate) struct Envelope {
-    #[yaserde(rename = "Body")]
+    #[yaserde(rename = "Body", prefix = "soapenv")]
     pub(crate) body: Body,
 }
 #[derive(YaDeserialize, YaSerialize)]
-#[yaserde(
-prefix = "soapenv"
-)]
 pub(crate) struct Body {
-    #[yaserde(rename = "GetMessagesRequest")]
+    #[yaserde(rename = "GetMessagesRequest", prefix = "ns")]
     pub(crate) get_messages_request: GetMessagesRequest,
 }
 
 #[derive(YaDeserialize, YaSerialize)]
-#[yaserde(
-prefix = "ns"
-)]
 pub(crate) struct GetMessagesRequest {
-    #[yaserde(rename = "Expressions")]
+    #[yaserde(rename = "Expressions", prefix = "ns")]
     pub(crate) expressions: Vec<Expressions>,
 }
 #[derive(YaDeserialize, YaSerialize)]
-#[yaserde(
-prefix = "ns"
-)]
 pub(crate) struct Expressions {
-    #[yaserde(rename = "MessageId")]
+    #[yaserde(rename = "MessageId", prefix = "ns")]
     pub(crate) message_id: String,
-    #[yaserde(rename = "UserToken")]
+    #[yaserde(rename = "UserToken", prefix = "ns")]
     pub(crate) user_token: String
 }
 
